d1968623bee892f65d6b851a613510ecce941018
John Kuhn <johnkuhn@Johns-MacBook-Pro-2.local>
2017-05-22 22:44:57 -0400

    added cloud function for getting all articles

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,32 @@ var functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+// exports.addMessage = functions.https.onRequest((req, res) => {
+//   // Grab the text parameter.
+//   const original = req.query.text;
+//   // Push it into the Realtime Database then send a response
+//   admin.database().ref('/messages').push({original: original}).then(snapshot => {
+//     // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//     res.redirect(303, snapshot.ref);
+//   });
+// });
+
+exports.getAllArticles = functions.https.onRequest((req, res) => {
+
+	admin.database().ref('/articles').on('value', function(snapshot){
+		res.send(snapshot.val());
+	});
+
+});
+
+exports.getArticleByName = functions.https.onRequest((req, res) => {
+
+	var name = req.query.name;
+
+	res.send()
+
+});
