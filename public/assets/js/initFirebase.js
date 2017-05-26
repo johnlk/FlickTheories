e974843b599284e4a5c117e5e242248c1508758c
johnlk <jlkuhn@purdue.edu>
2017-05-25 21:16:04 -0400

    make a separte js file for initializing firebase

---
+++ public/assets/js/initFirebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCwNaBXbDWunUuT5HuMzxgQRNF0ByOiq04",
+	authDomain: "moviethoughts-d188d.firebaseapp.com",
+	databaseURL: "https://moviethoughts-d188d.firebaseio.com",
+	projectId: "moviethoughts-d188d",
+	storageBucket: "moviethoughts-d188d.appspot.com",
+	messagingSenderId: "923578276783"
+};
+firebase.initializeApp(config);
